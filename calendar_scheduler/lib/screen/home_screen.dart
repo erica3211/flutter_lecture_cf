1bdc75960ea21b7638561a37c7b59cb484dd79c4
moonhyejee <erica3211@naver.com>
2024-12-28 15:28:27 +0900

    달력

---
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -0,0 +1,97 @@
+import 'package:calendar_scheduler/component/calendar.dart';
+import 'package:calendar_scheduler/component/custom_text_field.dart';
+import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
+import 'package:calendar_scheduler/component/today_banner.dart';
+import 'package:calendar_scheduler/const/color.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDay = DateTime.utc(
+      DateTime.now().year, DateTime.now().month, DateTime.now().day);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (_) {
+              return ScheduleBottomSheet();
+            },
+          );
+        },
+        backgroundColor: primaryColor,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Calendar(
+              focusedDay: DateTime(2024, 12, 1),
+              onDaySelected: onDayselected,
+              selectedDayPredicate: selectedDayPredicate,
+            ),
+            TodayBanner(
+              selectedDay: selectedDay,
+              taskCount: 0,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 16.0,
+                  right: 16.0,
+                  top: 16.0,
+                ),
+                child: ListView(
+                  children: [
+                    ScheduleCard(
+                      startTime: DateTime(
+                        2024,
+                        12,
+                        26,
+                        11,
+                      ),
+                      endTime: DateTime(
+                        2024,
+                        12,
+                        26,
+                        12,
+                      ),
+                      content: '플러터 공부하기',
+                      color: Colors.blue,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onDayselected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      this.selectedDay = selectedDay;
+    });
+  }
+
+  bool selectedDayPredicate(DateTime date) {
+    if (selectedDay == null) {
+      return false;
+    }
+    return date.isAtSameMomentAs(selectedDay!);
+  }
+}
